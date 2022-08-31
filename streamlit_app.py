a57796486e49924673f0d2249fb0bc43ab064df3
ukp2 <urszula.kus-porada@capgemini.com>
2022-08-31 14:13:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,4 @@ streamlit.dataframe(fruits_to_show) # old: streamlit.dataframe(my_fruit_list)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json)
