419752a6dd72a190051ede8b9393e813e22c7a91
ukp2 <urszula.kus-porada@capgemini.com>
2022-08-31 14:28:36 +0200

    Update streamlit_app.py
    cleaning

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,9 @@ streamlit.dataframe(fruits_to_show) # old: streamlit.dataframe(my_fruit_list)
 # Fruity response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just print data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit"+"kiwi")
+#streamlit.text(fruityvice_response.json()) #just print data to the screen
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #pandas used to normalize
 streamlit.dataframe( fruityvice_normalized ) #wsadza znormalizowane w tabelke
-
-streamlit.text( fruityvice_normalized )  # just print data to the screen - test 1
-streamlit.dataframe( fruityvice_response.json() )#test 2
+#streamlit.text( fruityvice_normalized )  # just print data to the screen - test 1
+#streamlit.dataframe( fruityvice_response.json() )#test 2
