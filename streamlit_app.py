889d1ab069b71c263ab3f5161ceb273d81ee5a67
ukp2 <urszula.kus-porada@capgemini.com>
2022-08-31 14:22:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,5 +27,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json()) #just print data to the screen
 
-streamlit.text(pandas.json_normalize(fruityvice_response.json())  #normalized version 
-#srteamlit.dataframe(pandas.json_normalize(fruityvice_response.json()) #df
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(pandas.json_normalize(fruityvice_response.json()) )  #normalized version 
