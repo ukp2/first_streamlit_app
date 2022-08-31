00edd54b464ca07ed8852ca5597c174d6817d987
ukp2 <urszula.kus-porada@capgemini.com>
2022-08-31 14:43:59 +0200

    Update streamlit_app.py
    back

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,10 +20,3 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show) # old: streamlit.dataframe(my_fruit_list)
-
-# Fruity response
-streamlit.header("Fruityvice Fruit Advice!")
-import requests
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #pandas used to normalize
-#streamlit.dataframe( fruityvice_normalized ) #wsadza znormalizowane w tabelke
