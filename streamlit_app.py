9ec96ae403b9d4f8a8117bdd33abc6abb6f89e74
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-01 11:36:20 +0200

    Update streamlit_app.py
    try-except

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,26 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show) # old: streamlit.dataframe(my_fruit_list)
 
 # Fruity response
+## Odl section - replaced with try-except : 
+# streamlit.header("Fruityvice Fruit Advice!")
+# fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# streamlit.write('The user entered ', fruit_choice)
+# import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #pandas used to normalize
+# streamlit.dataframe( fruityvice_normalized ) #wsadza znormalizowane w tabelke
+
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #pandas used to normalize
-streamlit.dataframe( fruityvice_normalized ) #wsadza znormalizowane w tabelke
-
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit")
+  else: 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe( fruityvice_normalized )
+except URLError as e:
+  streamlit.error()
 
 
 #don't run anything past here while we troubleshoot
