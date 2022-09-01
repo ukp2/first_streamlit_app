243442135f18838a3c9fdb54cd3ceb9206273599
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-01 13:13:27 +0200

    Update streamlit_app.py
    spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,14 +55,13 @@ def get_fruityvice_data(this_fruit_choice):
   return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
-  try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-      streamlit.error("Please select a fruit")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+            streamlit.error("Please select a fruit")
   else:
-    back_from _function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from _function)
-
+      back_from _function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from _function)
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
