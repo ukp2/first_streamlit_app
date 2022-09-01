c00d806087f2b5ff53a6887dacdc3d5cdce0ce41
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-01 10:19:53 +0200

    Update streamlit_app.py
    snowflake metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,14 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) #pandas used to normalize
 streamlit.dataframe( fruityvice_normalized ) #wsadza znormalizowane w tabelke
 
+
+
+
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
