ab8b2a002bf0d17f931a8cf8f172868c0cd47db6
ukp2 <urszula.kus-porada@capgemini.com>
2022-09-01 14:12:54 +0200

    Update streamlit_app.py
     my_cnx.close()  #to close connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,10 +79,11 @@ def get_fruit_load_list():
       with my_cnx.cursor() as my_cur:
             my_cur.execute("SELECT * from fruit_load_list")
             return my_cur.fetchall()
- #add a button
+ #add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       my_data_row = get_fruit_load_list()
+      my_cnx.close()  #to close connection 
       streamlit.dataframe(my_data_row) 
       
       
@@ -104,6 +105,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
       back_from_function = insert_row_snowflake(add_my_fruit)
+      my_cnx.close()  #to close connection 
       streamlit.text(back_from_function)
 
       
